2c21328f3a50b19acfa7811789ddb0b635363f3e
hishemok <102466484+hishemok@users.noreply.github.com>
2022-03-27 13:40:24 +0200

    Update 1a_IV.py

--- Molecular_Dynamics/Part1/1a_IV.py
+++ Molecular_Dynamics/Part1/1a_IV.py
@@ -1,6 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+epsilon , sigma = 1 ,1
+n = 1000
+r = np.linspace(0.9,3,n+1) 
 
-
-
+def u(r,epsilon ,sigma):
+  u = 4∗epsilon∗( (sigma/r)∗∗12 − (sigma/r)∗∗6 ) 
+  return u
+ 
 
 #up until this point the code is from 1a_I.py
 
