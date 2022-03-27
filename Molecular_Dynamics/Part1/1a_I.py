b361015ed138aa082ccfbe7462d68a467996823c
hishemok <102466484+hishemok@users.noreply.github.com>
2022-03-27 17:30:41 +0200

    Update 1a_I.py

--- Molecular_Dynamics/Part1/1a_I.py
+++ Molecular_Dynamics/Part1/1a_I.py
@@ -1,16 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib . pyplot as plt
-epsilon , sigma = 1 ,1
-n = 1000
-r = np.linspace(0.9,3,n+1) 
 
-def u(r,epsilon ,sigma):
-  u = 4∗epsilon∗( (sigma/r)∗∗12 − (sigma/r)∗∗6 ) 
-  return u
- 
-U = u(r,epsilon ,sigma)
-  
-plt.plot( r , U )
-plt.xlabel( ' r ' ) 
-plt.ylabel( 'u( r ) ' )
+epsilon = 1
+sigma = 1
+r = np.linspace(0.9,3,300)
+U = lambda r,sigma:4*epsilon*((sigma/r)**(12)-(sigma/r)**(6))
+
+plt.plot(r,U(r,sigma))
 plt.show()
